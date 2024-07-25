26051ff358ab03b7c0b0769f479a43adcbdda8e8
Johnathan Gutierrez-Diaz <116782055+JohnathanGD@users.noreply.github.com>
2024-07-25 19:12:33 -0400

    Update food.js - 07/25/2024

--- ios/components/food.js
+++ ios/components/food.js
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    alignItems: "center",
+    alignItems: "flex-start",
   },
   row: {
     flexDirection: "row",
